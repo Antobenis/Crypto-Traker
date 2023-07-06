3b92cd590a0411e24a23c6b68c539ccb6bf6887a
anto <antobenis100@gmail.com>
2023-07-06 11:35:42 +0530

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import './App.css';
 import ShowCoins from './components/ShowCoins'
-import {useState,useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { TopSixCoins } from './components/TopSixCoins';
+import axios from 'axios';
 
 function App() {
-  const [coinsData,setCoinsData]=useState([])
-  const [search,setSearch]=useState("")
-  const top6=coinsData.splice(0,6)
+  const [coinsData, setCoinsData] = useState([])
+  const [search, setSearch] = useState("")
+  const top6 = coinsData.splice(0, 6)
 
-  const makeRequest=async()=>{
-    try{
-      const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-      const data=await response.json()
-      setCoinsData(data)
-    }
-    catch(err){
-        console.log("Error while fetching data",err)
-    }
-  }
+  // const makeRequest=async()=>{
+  //   try{
+  //     const response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+  //     const data=await response.json()
+  //     setCoinsData(data)
+  //   }
+  //   catch(err){
+  //       console.log("Error while fetching data",err)
+  //   }
+  // }
 
-  useEffect(()=>{
-    makeRequest()
-  },[search])
+  useEffect(() => {
+    axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+      .then(function (response) {
+        // handle success
+        console.log(response?.data);
+        setCoinsData(response?.data)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }, [search])
 
-  let coinFilter=coinsData.filter((ele)=>{
-    if (ele.name.toLowerCase().includes(search.toLowerCase())){
+  let coinFilter = coinsData.filter((ele) => {
+    if (ele.name.toLowerCase().includes(search.toLowerCase())) {
       return ele
     }
   })
@@ -43,7 +53,7 @@ function App() {
           }}
         />
       </div>
-      {search.length>0?<ShowCoins coinsData={coinFilter}/> :null}
+      {search.length > 0 ? <ShowCoins coinsData={coinFilter} /> : null}
       {top6.length ? <TopSixCoins topSix={top6} /> : null}
       <ShowCoins coinsData={coinFilter} />
 
